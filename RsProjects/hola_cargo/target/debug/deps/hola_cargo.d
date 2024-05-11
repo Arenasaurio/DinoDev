fabf4c4e5a52cefbf81522b62b99921ad5949790
Arenasaurio <arenasaurio03@gmail.com>
2024-05-11 13:22:16 -0600

    iguana

---
+++ RsProjects/hola_cargo/target/debug/deps/hola_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Csera\DinoDev\RsProjects\hola_cargo\target\debug\deps\hola_cargo.exe: src\main.rs
+
+C:\Users\Csera\DinoDev\RsProjects\hola_cargo\target\debug\deps\hola_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ RsProjects/hola_cargo/target/debug/hola_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Csera\DinoDev\RsProjects\hola_cargo\target\debug\hola_cargo.exe: C:\Users\Csera\DinoDev\RsProjects\hola_cargo\src\main.rs
